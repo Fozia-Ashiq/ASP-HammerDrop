8b31f9508de6fbc71d91d3d523c1d1b08af28a07
Fozia-Ashiq <foziaashiq768@gmail.com>
2025-09-05 16:43:08 +0500

    Initial commit - ASP.NET Core MVC project

--- HammerDrop Auction app/obj/Debug/net8.0/HammerDropAuctionapp.AssemblyInfo.cs
+++ HammerDrop Auction app/obj/Debug/net8.0/HammerDropAuctionapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HammerDropAuctionapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42cf0b5639cb5f204b08a0c11d129137df1e7a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("HammerDropAuctionapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HammerDropAuctionapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
